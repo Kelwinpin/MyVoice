197b672218e893d2a8632a64cf0a70c693963823
Kelwin <kelwincomw@gmail.com>
2024-10-15 23:37:47 -0300

    add db config

---
+++ src/database/config/index.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '123456',
+  database: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3306,
+  dialect: 'postgres',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+};
+
+module.exports = config;
