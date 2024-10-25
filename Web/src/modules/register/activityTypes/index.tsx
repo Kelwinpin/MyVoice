199518ab70a507ae03c839aa49178d46bf3e13fe
Kelwinpin <kelwincomw@gmail.com>
2024-10-25 09:51:14 -0300

    add refetch

--- Web/src/modules/register/activityTypes/index.tsx
+++ Web/src/modules/register/activityTypes/index.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Register from "..";
 import { Dialog } from "@/components/hand/Dialog";
 import { useToast } from "@/hooks/use-toast";
@@ -19,87 +19,6 @@ interface IActivityType {
   id: number
   name: string
 }
-
-let callApi = 0
-
-
-const columns: ColumnDef<IActivityType>[] = [
-    {
-      accessorKey: "id",
-      header: "ID",
-    },
-    {
-      accessorKey: "name",
-      header: "Nome",
-    },
-    {
-        accessorKey: " ",
-        header: " ",
-        cell: ({row}) => {    
-
-          const [visible, setVisible] = useState(false)
-
-          const { toast } = useToast()
-
-          const mutation = useMutation({
-              mutationFn: (id:number) => fetch(`http://localhost:3000/activityTypes/${id}`, {
-                  method: 'DELETE',
-                  headers: {
-                      'Content-Type': 'application/json',
-                  },
-              }).then(res => res.json()),
-              onSuccess: () => {
-                  toast({
-                      variant: "success",
-                      title: "Success",
-                      description: "Activity Type deleted successfully",
-                      duration: 3000
-                  })
-                  callApi++
-                  setTimeout(() => setVisible(false), 2000)
-              },
-              onError: (error) => {
-                  toast({
-                      variant: "destructive",
-                      title: "Error",
-                      description: error.message
-                  })
-              }
-          })
-
-          return (
-            <>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="link" className="h-8 w-8 p-0">
-                    <MoreHorizontal className="h-4 w-4" />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="start">
-                  <DropdownMenuItem className="cursor-pointer">
-                      <Pencil className="h-4 w-4" color="blue" /> Editar
-                  </DropdownMenuItem>
-                  <DropdownMenuItem className="cursor-pointer" onClick={() => setVisible(true)}>
-                    <Trash2 className="h-4 w-4"  color="red" /> Excluir
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-              <Dialog
-                title="Excluir Atividade"
-                description="Tem certeza que deseja excluir esta atividade?"
-                buttonTriggerText="Excluir"
-                buttonCancelText="Cancelar"
-                buttonConfirmText="Excluir"
-                isOpen={visible}
-                onConfirm={() => mutation.mutate(row.original.id)}
-                showTrigger={false}
-                onCancel={() => setVisible(false)}
-              />
-            </>
-          )
-        },
-      },
-]
 
 export default function ActivityTypes() {
     const query = useQuery({
@@ -110,9 +29,84 @@ export default function ActivityTypes() {
 
     const [visible, setVisible] = useState(false)
 
-    useEffect(() =>{
-      query.refetch()
-    }, [callApi])
+
+    const columns: ColumnDef<IActivityType>[] = [
+        {
+          accessorKey: "id",
+          header: "ID",
+        },
+        {
+          accessorKey: "name",
+          header: "Nome",
+        },
+        {
+            accessorKey: " ",
+            header: " ",
+            cell: ({row}) => {    
+
+              const [visible, setVisible] = useState(false)
+
+              const { toast } = useToast()
+
+              const mutation = useMutation({
+                  mutationFn: (id:number) => fetch(`http://localhost:3000/activityTypes/${id}`, {
+                      method: 'DELETE',
+                      headers: {
+                          'Content-Type': 'application/json',
+                      },
+                  }).then(res => res.json()),
+                  onSuccess: () => {
+                      toast({
+                          variant: "success",
+                          title: "Success",
+                          description: "Activity Type deleted successfully",
+                          duration: 3000
+                      })
+                      query.refetch()
+                      setTimeout(() => setVisible(false), 2000)
+                  },
+                  onError: (error) => {
+                      toast({
+                          variant: "destructive",
+                          title: "Error",
+                          description: error.message
+                      })
+                  }
+              })
+
+              return (
+                <>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="link" className="h-8 w-8 p-0">
+                        <MoreHorizontal className="h-4 w-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="start">
+                      <DropdownMenuItem className="cursor-pointer">
+                          <Pencil className="h-4 w-4" color="blue" /> Editar
+                      </DropdownMenuItem>
+                      <DropdownMenuItem className="cursor-pointer" onClick={() => setVisible(true)}>
+                        <Trash2 className="h-4 w-4"  color="red" /> Excluir
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                  <Dialog
+                    title="Excluir Atividade"
+                    description="Tem certeza que deseja excluir esta atividade?"
+                    buttonTriggerText="Excluir"
+                    buttonCancelText="Cancelar"
+                    buttonConfirmText="Excluir"
+                    isOpen={visible}
+                    onConfirm={() => mutation.mutate(row.original.id)}
+                    showTrigger={false}
+                    onCancel={() => setVisible(false)}
+                  />
+                </>
+              )
+            },
+          },
+    ]
     
 
     return (
